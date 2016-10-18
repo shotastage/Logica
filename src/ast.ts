0a820974809de387251f6e79a83467666d99c838
Shota Shimazu <hornet.live.mf@gmail.com>
2016-10-19 01:45:23 +0900

    Define langugae def.

--- src/ast.ts
+++ src/ast.ts
@@ -1,32 +1,48 @@
-let lexer = Parsect.makeTokenParser({
-    commentStart:       '/*',
-    commsentEnd:         '*/',
-    commentInLine:        '//',
-    nestedComments:     true,
-    identStart:         /[_$a-zA-Z]/,
-    identLetter:        /[_$a-zA-Z0-9]/,
-    opStart:            /[+\-*\/=!$%&\^~@?_><:|\\.]/,
-    opLetter:           /[+\-*\/=!$%&\^~@?_><:|\\.]/,
-    reservedNames: [ // Reserved words
-        "def",
-        "return",
-        "if",
-        "switch",
-        "for",
-        "while",
-        "when",
-        "class",
-        "struct",
-        "enum",
-        "let",
-        "var"
-    ],
-    reservedOpNames: [ // Reserved operators
-        "+",
-        "-",
-        "++",
-        "--",
-        "+@prefix"
-    ],
-    caseSensitive:      true
-});
+module CSSScriptAst {
+
+    export interface langugaeDefinition {
+        reservedNames: [
+            "def", "return",                // For function
+            "if", "elif", "else",           // For if
+            "switch", "case",               // For switch
+            "for", "in",                    // For for
+            "while", "do",                  // For while
+            "try", "catch",                 // For error handling
+            "when", "break",                // For when
+            "class", "struct", "enum",      // For object oriented programming
+            "let", "var",                   // For varibales and constnats
+            "import", "as", "from",         // For import styles or modules
+            "export", "module", "package"   // For export styles or modules
+        ];
+
+        AccessModifiers: [
+            "public", 
+            "private", "fileprivate"
+        ];
+
+        Operetors: [
+            "+", "-", "*", "/", 
+            "++", "--",
+            "+=", "-=", "=+", "=-",
+            ">", "<", "<=", ">=", "=>", "=<"
+        ];
+
+        Types: [
+            "int", "double", "number", "bool", "string", // Basic Types
+            "pixle", "color", "selector", "url"          // CSSScript Types
+        ];
+
+        Symbols: [
+            "!", "?",           // Optional Types
+            "&", "|",           // If syntax
+            "@"                 // CSS Object
+        ];
+
+        Letters: [
+            "0123456789",                       // Numbers
+            "abcdefghijklmnopqrstuvwxyz",       // Alphabets
+            "ABCDEFGHIJKLMNOPQRSTUVWXYZ",       // Alphabets
+            "!#$%&(-=^~¥|@`[]{};+:*,<.>/?_"     // Symbols
+        ];
+    }
+}
