a7f6b16da9292edd855e66877156526ec64d92ac
Shota Shimazu <hornet.live.mf@gmail.com>
2016-10-19 00:17:10 +0900

    Add concept CSSS level 0.

--- src/index.ts
+++ src/index.ts
@@ -5,19 +5,33 @@
 // Licensed under the Apache v2, see LICENSE for detail.
 //
 
+
+// CommandLine Tools (CLI) for CSSS.
 import commands = require("commander");
 
 
 commands
+
+  // Show version of CSSS.
   .version('0.0.1')
+
+  // Create a new CSSS project, but it not nessesary.
   .command('init', 'create a new CSSS prject.')
+
+  // Compile or build csss script.
   .command('compile [file]', 'compile csss file.')
+  .command('watch', 'watch files.')
+  .command('build', 'build CSSS package.')
+
+  // Manage CSSS plugins
   .command('install [pulgin]', 'install extra plugin.')
   .command('installed', 'show all installed plugins.')
   .command('remove [plugin]', 'remove plugin.')
-  .command('watch', 'watch files.')
-  .command('build', 'build CSSS package.')
+
+  // Test command
   .command('dammy', 'test run.')
+
+
   .option('-h, --help', 'show help.')
   .option('-g, --global--install', 'install plugin globally.')
   .parse(process.argv)
