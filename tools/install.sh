afc5f31f56329232a23a77579dbb02e28cde9f76
Shota Shimazu <hornet.live.mf@gmail.com>
2016-10-20 16:38:28 +0900

    Update installer.

--- tools/install.sh
+++ tools/install.sh
@@ -5,6 +5,14 @@
 install () {
     check () {
         function node() {
+            if type "node" > /dev/null 2>&1; then
+			    echo "Node.js is installed."
+		    else
+			    : 
+		    fi
+        }
+
+        function logica() {
             
         }
 
