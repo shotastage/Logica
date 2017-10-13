3c3cbb080ec2a1825b399b88a14353e264cffd67
Shota Shimazu <hornet.live.mf@gmail.com>
2017-10-14 01:58:38 +0900

    Update.

---
+++ src/define.ts
@@ -0,0 +1,39 @@
+module Logica
+{
+
+
+    export interface SyntaxDefinision
+    {
+        ReservedNames: [
+            "func",
+            "return",
+            "if",
+            "elif",
+            "else",            
+            "switch",
+            "case",                        
+            "for",
+            "in",                        
+            "try",
+            "catch",                          
+            "class",
+            "struct",
+            "enum", 
+            "protocol",    
+            "let",
+            "var",                           
+            "import",
+            "as",
+            "from",                 
+            "export",
+            "module",
+            "typealias",
+            "while", 
+
+            // ---------------------------------------------------------------//
+            "insert", 　　　　 　                                               //
+            "interface"                                                       //
+            // ---------------------------------------------------------------//
+        ];
+    }
+}
